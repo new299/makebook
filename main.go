324af787b6ce68f886f7c1b77e5ace757af0f28b
Ubuntu <ubuntu@ip-10-167-10-161.us-west-1.compute.internal>
2012-10-24 22:22:49 +0000

    added bootstrap (which is servered elsewhere)

--- main.go
+++ main.go
@@ -18,13 +18,13 @@ type Page struct {
 }
 
 func makebook(bookname string) error {
-	filename := bookname + ".epub"
 
 	search := strings.Replace(bookname, " ", "%20", -1)
 
   cmdargs := "http://en.wikipedia.org/w/index.php?title=Special%3ASearch&profile=default&search=" + search + "&fulltext=Search"
   cmd := exec.Command("curl",cmdargs)
   out,e := cmd.Output()
+	if e != nil { fmt.Println(e) }
 
 	s := string(out);
   re := regexp.MustCompile(`mw-search-result-heading'><a href="[A-Za-z0-9_//]+"`)
@@ -34,6 +34,10 @@ func makebook(bookname string) error {
 
   exec.Command("mkdir",bookname).Output()
 
+  fmt.Println("writing: " + bookname + ".html");
+	indexfile, err := os.Create(bookname + ".html")
+  if err != nil {fmt.Println(err);}
+
   for i := 0; i < len(searchres); i++ {
 		current := string(searchres[i][0])
 
@@ -41,21 +45,21 @@ func makebook(bookname string) error {
   	url := "http://en.wikipedia.org/w/index.php?title=" + current[trimlen:len(current)-1] + "&printable=yes"
 
     fmt.Println(url);
-    wgetcmd := exec.Command("wget","-p","-k",url,"-O",pagename + ".html")
+    wgetcmd := exec.Command("wget","--wait=1","-k","-E","-H","-K","-e","robots=off","-p",url)
 		wgetcmd.Dir = bookname
 		wgetout,e := wgetcmd.CombinedOutput()
 		fmt.Println(string(wgetout))
     if e != nil { fmt.Println(e); }
+
+    io.WriteString(indexfile,"<a href=\"");
+    io.WriteString(indexfile,bookname + "/en.wikipedia.org/w/index.php?title=" + pagename + "&printable=yes.html");
+		io.WriteString(indexfile,"\">" + pagename + "</a><br>");
   }
 
-  if e != nil {fmt.Println(e);}
+  indexfile.Close()
 
-  fmt.Println("writing: " + filename);
-	f, err := os.Create(filename)
-  if err != nil { fmt.Println(err); return err }
-  n, err := io.WriteString(f, "blahblahblah")
-  if err != nil { fmt.Println(n, err); return err }
-  f.Close()
+  ebookcmd := exec.Command("ebook-convert",bookname + ".html",bookname + ".epub")
+	ebookcmd.Output()
 
   return err
 }
@@ -73,30 +77,39 @@ func loadPage(title string) (*Page, error) {
 func downHandler(w http.ResponseWriter, r *http.Request, title string) {
 
   booktitle := r.FormValue("booktitle")
+	w.Header().Set("Content-Type", "application/epub+zip")
 
-  makebook(booktitle)
+	w.Header().Set("Content-Disposition", "attachment; filename=\"" + booktitle +".epub\"")
 
   bookdata, e := ioutil.ReadFile(booktitle + ".epub")
   if e != nil {
     fmt.Println("read error: " + booktitle)
+  	makebook(booktitle)
+  	bookdata, e := ioutil.ReadFile(booktitle + ".epub")
+		if e != nil { fmt.Println(e) }
+
+		w.Write(bookdata);
+		return
   }
 
-  w.Header().Set("Content-Type", "application/xhtml+xml")
-
 	w.Write(bookdata);
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request, title string) {
+	http.Redirect(w, r, "/form/", http.StatusFound)
 }
 
 func formHandler(w http.ResponseWriter, r *http.Request, title string) {
 
   fmt.Println("formhandler");
-	p, err := loadPage(title)
+	p, err := loadPage("form")
 	if err != nil {
 		p = &Page{Title: title}
 	}
 	renderTemplate(w, "form", p)
 }
 
-var templates = template.Must(template.ParseFiles("form.html", "down.html"))
+var templates = template.Must(template.ParseFiles("form.html"))
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	err := templates.ExecuteTemplate(w, tmpl+".html", p)
@@ -113,7 +126,11 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 	return func(w http.ResponseWriter, r *http.Request) {
 
     fmt.Println("Received path: "+r.URL.Path);
-		title := r.URL.Path[lenPath:]
+
+		title := string("/")
+		if len(r.URL.Path) > lenPath {
+			title = r.URL.Path[lenPath:]
+		}
 	//	if !titleValidator.MatchString(title) {
 //			http.NotFound(w, r)
 //			return
@@ -126,5 +143,6 @@ func main() {
   fmt.Println("Registering handlers");
 	http.HandleFunc("/down/", makeHandler(downHandler))
 	http.HandleFunc("/form/", makeHandler(formHandler))
+	http.HandleFunc("/"     , makeHandler(rootHandler))
 	http.ListenAndServe(":80", nil)
 }
