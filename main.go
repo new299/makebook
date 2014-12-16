de916f6c55c8cc65c57c35815dfb48089fc9cb33
Nava Whiteford <new@sgenomics.org>
2014-12-16 11:28:00 -0500

    added missing file

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"github.com/new299/makebook/sites"
+	"net/http"
+	"fmt"
+	"strings"
+)
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		fmt.Println("Received host: "+r.Host)
+    fmt.Println("Received path: "+r.URL.Path)
+
+		title := string("/")
+		if len(r.URL.Path) > 2 {
+			path := strings.TrimRight(r.URL.Path, "/")
+			last := path[strings.LastIndex(path,"/")+1:]
+			title = last
+			fmt.Println("title: "+title);
+		}
+//	if !titleValidator.MatchString(title) {
+//			http.NotFound(w, r)
+//			return
+//		}
+		fn(w, r, title)
+	}
+}
+
+func main() {
+  fmt.Println("Registering handlers");
+	http.HandleFunc("makebook.41j.com/down/", makeHandler(sites.MakebookDownHandler))
+	http.HandleFunc("makebook.41j.com/form/", makeHandler(sites.MakebookFormHandler))
+	http.HandleFunc("makebook.41j.com/"     , makeHandler(sites.MakebookRootHandler))
+	http.HandleFunc("/down"   , makeHandler(sites.GraphJSONDownHandler))
+	http.HandleFunc("/graph"  , makeHandler(sites.GraphJSONFormHandler))
+	http.HandleFunc("graph.41j.com/"        , makeHandler(sites.GraphJSONRootHandler))
+	http.ListenAndServe(":8090", nil)
+}
